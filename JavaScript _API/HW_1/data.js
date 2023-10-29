59568656995968916cc4f984d457a7d14dd5d941
Oleg-Tkachev <110471834+Oleg-Tkachev@users.noreply.github.com>
2023-10-29 23:57:49 +0300

    Add files via upload

---
+++ JavaScript _API/HW_1/data.js
@@ -0,0 +1,32 @@
+export const works = [
+  {
+    name: "Занятие 1",
+    time: "10:00 AM",
+    maxParticipants: 20,
+    currentParticipants: 10,
+  },
+  {
+    name: "Занятие 2",
+    time: "2:00 PM",
+    maxParticipants: 15,
+    currentParticipants: 12,
+  },
+  {
+    name: "Занятие 3",
+    time: "3:30 PM",
+    maxParticipants: 18,
+    currentParticipants: 6,
+  },
+  {
+    name: "Занятие 4",
+    time: "5:00 PM",
+    maxParticipants: 25,
+    currentParticipants: 14,
+  },
+  {
+    name: "Занятие 5",
+    time: "7:30 PM",
+    maxParticipants: 12,
+    currentParticipants: 8,
+  },
+];
